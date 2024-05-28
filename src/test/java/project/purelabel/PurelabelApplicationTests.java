ed78dbca83071ffb485ec8375a078abe6b450435
kang yeon joo <rkdduswndi@naver.com>
2024-05-28 16:19:58 +0900

    feat: git init

---
+++ src/test/java/project/purelabel/PurelabelApplicationTests.java
@@ -0,0 +1,13 @@
+package project.purelabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurelabelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
