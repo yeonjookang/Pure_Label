dd181d52395e91fe3789091989d6314252c5400e
kang yeon joo <rkdduswndi@naver.com>
2024-10-26 23:49:23 +0900

    fix: 유저 정보 조회 버그 수정

--- src/main/java/project/purelabel/user/api/request/UserInfoRequestDto.java
+++ src/main/java/project/purelabel/user/api/request/UserInfoRequestDto.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfoRequestDto {
     @NotNull
     Long pk;
--- src/main/java/project/purelabel/user/application/response/UserInfoResponseDto.java
+++ src/main/java/project/purelabel/user/application/response/UserInfoResponseDto.java
@@ -2,14 +2,14 @@ package project.purelabel.user.application.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 import project.purelabel.user.domain.value.Gender;
 import project.purelabel.user.domain.value.SkinType;
 import project.purelabel.user.domain.value.SkinWorry;
 
 import java.util.List;
 
-@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class UserInfoResponseDto {
     private String name;
     private String id;
