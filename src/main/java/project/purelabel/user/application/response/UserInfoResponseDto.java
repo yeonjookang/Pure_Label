79ecc8cf354a303350e0e817238caa9beae474f0
kang yeon joo <rkdduswndi@naver.com>
2024-10-26 23:57:14 +0900

    fix: 유저 정보 조회 버그 수정

--- src/main/java/project/purelabel/user/application/response/UserInfoResponseDto.java
+++ src/main/java/project/purelabel/user/application/response/UserInfoResponseDto.java
@@ -2,12 +2,14 @@ package project.purelabel.user.application.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import project.purelabel.user.domain.value.Gender;
 import project.purelabel.user.domain.value.SkinType;
 import project.purelabel.user.domain.value.SkinWorry;
 
 import java.util.List;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class UserInfoResponseDto {
